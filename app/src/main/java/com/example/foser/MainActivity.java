ce8d2e3b108cd79f7104397be15ce02b7ccb34f9
kapir1234 <kcpr.knp@gmail.com>
2020-11-23 16:59:07 +0100

    Lab7

--- app/src/main/java/com/example/foser/MainActivity.java
+++ app/src/main/java/com/example/foser/MainActivity.java
@@ -7,13 +7,43 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonStart = findViewById(R.id.buttonStart);
+        Button buttonStop = findViewById(R.id.buttonStop);
+        Button buttonRestart = findViewById(R.id.buttonRestart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickStart(view);
+            }
+        });
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickStop(view);
+            }
+        });
+
+        buttonRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickRestart(view);
+            }
+        });
     }
 
     @Override
@@ -29,5 +59,18 @@ public class MainActivity extends AppCompatActivity {
             case R.id.itemExit: finishAndRemoveTask(); return true;
             default: return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void clickStart(View view) {
+        Toast.makeText(this,"Start",Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickStop(View view) {
+        Toast.makeText(this,"Stop",Toast.LENGTH_SHORT).show();
+    }
+
+    public void clickRestart(View view) {
+        clickStop(view);
+        clickStart(view);
     }
 }
