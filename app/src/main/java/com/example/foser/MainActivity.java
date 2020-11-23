0fb68966da19900845737c7f263895295744ac83
kapir1234 <kcpr.knp@gmail.com>
2020-11-23 16:43:27 +0100

    Lab7

---
+++ app/src/main/java/com/example/foser/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.foser;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemSettings: startActivity(new Intent(this,SettingsActivity.class)); return true;
+            case R.id.itemExit: finishAndRemoveTask(); return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+}
